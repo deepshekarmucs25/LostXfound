7fb8c3ea5a08abdc344fe327212de7d880a2b8ea
deepshekarmucs25 <deepshekarmu.cs25@bmsce.ac.in>
2026-05-08 07:22:48 +0530

    Update config.ts

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -1,10 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
-
 import { env } from "@/env";
 import { db } from "@/server/db";
-
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg", // or "pg" or "mysql"
@@ -16,9 +13,8 @@ export const auth = betterAuth({
 		github: {
 			clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
 			clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
-			redirectURI: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback/github`,
+			redirectURI: `${env.NEXT_PUBLIC_APP_URL}/api/auth/callback/github`,
 		},
 	},
 });
-
 export type Session = typeof auth.$Infer.Session;
