a468402dc9ecae08117cf11762a1bfb12066c608
deepshekarmucs25 <deepshekarmu.cs25@bmsce.ac.in>
2026-05-08 07:22:22 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,11 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-
 export const env = createEnv({
 	/**
 	 * Specify your server-side environment variables schema here. This way you can ensure the app
 	 * isn't built with invalid env vars.
 	 */
+	
 	server: {
 		BETTER_AUTH_SECRET:
 			process.env.NODE_ENV === "production"
@@ -14,21 +14,18 @@ export const env = createEnv({
 		BETTER_AUTH_GITHUB_CLIENT_ID: z.string(),
 		BETTER_AUTH_GITHUB_CLIENT_SECRET: z.string(),
 		DATABASE_URL: z.string().url(),
-		NEXT_PUBLIC_APP_URL: z.string().url(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
 	},
-
 	/**
 	 * Specify your client-side environment variables schema here. This way you can ensure the app
 	 * isn't built with invalid env vars. To expose them to the client, prefix them with
 	 * `NEXT_PUBLIC_`.
 	 */
 	client: {
-		// NEXT_PUBLIC_CLIENTVAR: z.string(),
+		NEXT_PUBLIC_APP_URL: z.string().url(),
 	},
-
 	/**
 	 * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
 	 * middlewares) or client-side so we need to destruct manually.
